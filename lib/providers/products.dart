07378c05d4df017a5029ff205875b6827c9a701c
a7md0 <a7m3d699@gmail.com>
2021-02-24 00:06:32 +0300

    Create products provider

---
+++ lib/providers/products.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'package:shop_app/models/product.dart';
+
+class Products with ChangeNotifier {
+  List<Product> _items = [];
+
+  List<Product> get items => [..._items];
+}
