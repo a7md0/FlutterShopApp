a2c74c6be8fd8ada57ff38d4c96e6772ee13de9a
a7md0 <a7m3d699@gmail.com>
2021-02-24 00:14:44 +0300

    Move dummy data to provider and extract grid widget and use provider

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:shop_app/providers/products.dart';
+
+import 'package:shop_app/widgets/product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<Products>(context);
+    final products = provider.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, idx) => ProductItem(products[idx]),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
