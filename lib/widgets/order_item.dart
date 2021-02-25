ff94796cc47424f7d90f09a5212c1ab1dce3b002
a7md0 <a7m3d699@gmail.com>
2021-02-25 16:52:40 +0300

    Order item complete

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,12 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import 'package:shop_app/providers/orders.dart' as Orders;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final Orders.OrderItem orderItem;
 
   OrderItem(this.orderItem);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +24,46 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${orderItem.amount}'),
+            title: Text('\$${widget.orderItem.amount}'),
             subtitle: Text(
-              DateFormat('dd-MM-yyyy').format(orderItem.createdAt),
+              DateFormat('dd-MM-yyyy').format(widget.orderItem.createdAt),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              height: min(widget.orderItem.products.length * 20.0 + 10.0, 100),
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              child: ListView.builder(
+                itemCount: widget.orderItem.products.length,
+                itemBuilder: (ctx, idx) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.orderItem.products[idx].title,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '${widget.orderItem.products[idx].quantity}x \$${widget.orderItem.products[idx].price}',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
         ],
       ),
     );
