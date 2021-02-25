a3e2780d57903e1d938f437a3ee95a06e279bcb5
a7md0 <a7m3d699@gmail.com>
2021-02-25 23:30:07 +0300

    User prod item navigate to edit product

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/providers/product.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
 
 class UserProductItem extends StatelessWidget {
   final Product product;
@@ -20,7 +21,10 @@ class UserProductItem extends StatelessWidget {
             IconButton(
               icon: Icon(Icons.edit),
               color: Theme.of(context).primaryColor,
-              onPressed: () {},
+              onPressed: () => Navigator.of(context).pushNamed(
+                EditProductScreen.routeName,
+                arguments: product.id,
+              ),
             ),
             IconButton(
               icon: Icon(Icons.delete),
