1aa1eb45e0d277655035fc70b26c9a2dbd877eb9
a7md0 <a7m3d699@gmail.com>
2021-02-25 16:23:03 +0300

    cart sc place order +empty cart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart' show Cart;
+import 'package:shop_app/providers/orders.dart';
 
 import 'package:shop_app/widgets/cart_item.dart';
 
@@ -42,7 +43,13 @@ class CartScreen extends StatelessWidget {
                   ),
                   FlatButton(
                     child: Text('ORDER'),
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                        cart.items.values.toList(),
+                        cart.itemsTotalAmount,
+                      );
+                      cart.reset();
+                    },
                     textColor: Theme.of(context).primaryColor,
                   )
                 ],
