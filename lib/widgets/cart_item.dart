fdc7d3708a00739edd732635873addb8e383e80a
a7md0 <a7m3d699@gmail.com>
2021-02-25 15:06:05 +0300

    CartItem dismissible

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:shop_app/providers/cart.dart' as Cart;
 
@@ -9,20 +10,38 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: EdgeInsets.all(5),
-              child: FittedBox(child: Text('\$${cartItem.price}')),
+    final cart = Provider.of<Cart.Cart>(context, listen: false);
+
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (dir) => cart.removeItem(cartItem.productId),
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: FittedBox(child: Text('\$${cartItem.price}')),
+              ),
             ),
+            title: Text(cartItem.title),
+            subtitle: Text('Total: \$${cartItem.price * cartItem.quantity}'),
+            trailing: Text('${cartItem.quantity} x'),
           ),
-          title: Text(cartItem.title),
-          subtitle: Text('Total: \$${cartItem.price * cartItem.quantity}'),
-          trailing: Text('${cartItem.quantity} x'),
         ),
       ),
     );
