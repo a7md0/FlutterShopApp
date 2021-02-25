4cfd5cf2d25bc25afa3651edd1a028bcf0ad2ec4
a7md0 <a7m3d699@gmail.com>
2021-02-25 16:41:10 +0300

    Use app drawer in shop and orders

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/orders.dart' show Orders;
+import 'package:shop_app/widgets/app_drawer.dart';
 
 import 'package:shop_app/widgets/order_item.dart';
 
@@ -16,6 +17,7 @@ class OrdersScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Orders'),
       ),
+      drawer: AppDrawer(),
       body: ListView.builder(
         itemCount: ordersProvider.orders.length,
         itemBuilder: (ctx, idx) => OrderItem(ordersProvider.orders[idx]),
