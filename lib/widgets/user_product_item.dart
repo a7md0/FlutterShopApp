32bcea65a3b9ef730affc9aa39a5f56dd75448df
a7md0 <a7m3d699@gmail.com>
2021-02-25 22:04:03 +0300

    Create user_product_item

---
+++ lib/widgets/user_product_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/providers/product.dart';
+
+class UserProductItem extends StatelessWidget {
+  final Product product;
+
+  UserProductItem(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(product.imageUrl),
+      ),
+      title: Text(product.title),
+      trailing: Row(
+        children: [
+          IconButton(
+            icon: Icon(Icons.edit),
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.delete),
+            color: Theme.of(context).errorColor,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
