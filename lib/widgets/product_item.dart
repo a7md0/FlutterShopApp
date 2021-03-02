aa1ae62072730422f13c8e047417dcb3111b72cd
a7md0 <a7m3d699@gmail.com>
2021-03-02 13:48:16 +0300

    Add FadeInImage

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -17,8 +17,9 @@ class ProductItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
-          child: Image.network(
-            product.imageUrl,
+          child: FadeInImage(
+            placeholder: AssetImage('assets/images/product-placeholder.png'),
+            image: NetworkImage(product.imageUrl),
             fit: BoxFit.cover,
           ),
           onTap: () => Navigator.of(context).pushNamed(
